a1c1a1cdb5b8edb5b6f1f7fde664299c801d726d
emrecoskun705 <emrecoskun705@gmail.com>
2023-11-21 23:02:22 +0300

    add serilog, api versioning and exception middleware

--- src/API/Unitagram.WebAPI/Program.cs
+++ src/API/Unitagram.WebAPI/Program.cs
@@ -1,9 +1,19 @@
+using Serilog;
 using Unitagram.Application;
 using Unitagram.Infrastructure;
 using Unitagram.Persistence;
 using Unitagram.Persistence.Data;
+using Unitagram.WebAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Serilog
+builder.Host.UseSerilog((context,  services,  loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration) // Read configuration settings from built-in IConfiguration (appsettings.json)
+        .ReadFrom.Services(services);// reads out current app's services and make them available to serilog
+});
 
 builder.Services.AddInfrastructureServices();
 builder.Services.AddApplicationServices();
@@ -16,6 +26,38 @@ if (app.Environment.IsDevelopment())
     await app.InitialiseDatabaseAsync();
 }
 
-app.MapGet("/", () => "Hello World!");
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseSerilogRequestLogging();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsProduction())
+{
+    app.UseHsts();
+    app.UseHttpsRedirection();
+}
+
+if (builder.Environment.IsDevelopment() || builder.Environment.IsProduction()) // TODO: remove production check later
+{
+    // app.UseDeveloperExceptionPage();
+    app.UseSwagger(); // creates endpoints for swagger.json
+    app.UseSwaggerUI(options =>
+    {
+        string swaggerJsonBasePath = string.IsNullOrWhiteSpace(options.RoutePrefix) ? "." : "..";
+        options.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "1.0");
+        options.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v2/swagger.json", "2.0");
+    });
+}
+
+app.UseRouting();
+app.UseCors("all");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
+
+/// <summary>
+/// make the auto-generated Program accessible programmatically
+/// </summary>
+public partial class Program { } 
