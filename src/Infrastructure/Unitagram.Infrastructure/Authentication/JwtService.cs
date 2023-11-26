91e657f1de914eb88d284ebcf7a952db2cdacbc9
emrecoskun705 <emrecoskun705@gmail.com>
2023-11-26 21:59:37 +0300

    add keycloak and authentication option classes

--- src/Infrastructure/Unitagram.Infrastructure/Authentication/JwtService.cs
+++ src/Infrastructure/Unitagram.Infrastructure/Authentication/JwtService.cs
@@ -12,47 +12,9 @@ namespace Unitagram.Infrastructure.Authentication;
 
 internal sealed class JwtService : IAccessTokenService
 {
-    private readonly JwtOptions _jwtOptions;
-
-    public JwtService(IOptions<JwtOptions> jwtOptions)
-    {
-        _jwtOptions = jwtOptions.Value;
-    }
-
+    
     public AccessTokenResponse CreateAccessToken(AccessTokenRequest request)
     {
-        DateTime expiration = DateTime.UtcNow.AddDays(Convert.ToDouble(_jwtOptions.ExpirationMinutes));
-
-        // Create an array of Claim objects representing the user's claims, such as their ID, name, email, etc.
-        var claims = new[]
-        {
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //JWT unique ID
-            new Claim(JwtRegisteredClaimNames.Sub, request.Username), //Subject (user id)
-            new Claim(JwtRegisteredClaimNames.Email, request.Email), //Subject (user email)
-            new Claim(JwtRegisteredClaimNames.Iat,
-                DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)), //Issued at (date and time of token generation)
-        };
-
-        var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Key));
-
-        var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
-
-        JwtSecurityToken tokenGenerator = new JwtSecurityToken(
-            _jwtOptions.Issuer,
-            _jwtOptions.Audience,
-            claims,
-            expires: expiration,
-            signingCredentials: signingCredentials
-        );
-
-        // Create a JwtSecurityTokenHandler object and use it to write the token as a string.
-        JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-        string token = tokenHandler.WriteToken(tokenGenerator);
-
-        return new()
-        {
-            AccessToken = token,
-            ExpiresIn = expiration
-        };
+        throw new NotImplementedException();
     }
 }
--- src/Infrastructure/Unitagram.Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/Unitagram.Infrastructure/DependencyInjection.cs
@@ -2,9 +2,13 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Unitagram.Application.Contracts.Common;
+using Unitagram.Application.Contracts.Identity;
 using Unitagram.Application.Models;
+using Unitagram.Infrastructure.Authentication;
 using Unitagram.Infrastructure.Localization;
 
 namespace Unitagram.Infrastructure;
@@ -13,37 +17,40 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,  IConfiguration configuration)
     {
-        services.Configure<JwtOptions>(configuration.GetSection(nameof(JwtOptions)));
+        services.AddAuthentication(configuration);        
         
+        services.AddLocalization(options => options.ResourcesPath = "Localization");
+        services.AddSingleton<ILocalizationService, LocalizationService>();
+        
+        return services;
+    }
+
+    private static void AddAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
         // Add JWT
         services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
             })
-            .AddJwtBearer(options =>
-            {
-                var jwtConfig = configuration.GetSection(nameof(JwtOptions)).Get<JwtOptions>()!;
-
-                options.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateAudience = true,
-                    ValidAudience = jwtConfig.Audience,
-                    ValidateIssuer = true,
-                    ValidIssuer = jwtConfig.Issuer,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(jwtConfig.Key)),
-                };
-            });
+            .AddJwtBearer();
+        
+        services.Configure<JwtOptions>(configuration.GetSection(nameof(JwtOptions)));
+        
+        services.Configure<AuthenticationOptions>(configuration.GetSection("Authentication"));
+        services.ConfigureOptions<JwtBearerOptionsSetup>();
+        services.Configure<KeycloakOptions>(configuration.GetSection("Keycloak"));
         
         services.AddAuthorization();
         
-        services.AddLocalization(options => options.ResourcesPath = "Localization");
-        services.AddSingleton<ILocalizationService, LocalizationService>();
+        services.AddHttpClient<IAccessTokenService, JwtService>((sp, client) => {
 
+            var opt = sp.GetRequiredService<IOptions<KeycloakOptions>>().Value;
+            client.BaseAddress = new Uri(opt.TokenUrl);
+
+        });
         
-        return services;
+        IdentityModelEventSource.ShowPII = true;
     }
 
 }
