6980ab4fd2b5a6241d296b23f8ec249c69da5ea6
emrecoskun705 <emrecoskun705@gmail.com>
2023-12-04 22:32:32 +0300

    add only access token

---
+++ src/Infrastructure/Unitagram.Infrastructure/Authentication/Models/AccessToken.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Unitagram.Infrastructure.Authentication.Models;
+
+public sealed class AccessToken
+{
+    [JsonPropertyName("access_token")]
+    public string Token { get; init; } = string.Empty;
+}
